507103dae6e98bee8f2eec74973de93984a6a86c
yegri <exsilver305@gmail.com>
2023-05-24 17:07:49 +0900

    스낵바 폴더 초기셋팅 완료

---
+++ snackbar/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Snack Bar',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  const MyPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Snack Bar'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
