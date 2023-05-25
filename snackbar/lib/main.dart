dd75de1ede9ccc9fde5637c7365a44295d37afef
yegri <exsilver305@gmail.com>
2023-05-25 10:52:20 +0900

    Feat : SnackBar 실습 완료

--- snackbar/lib/main.dart
+++ snackbar/lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Snack Bar',
       theme: ThemeData(primarySwatch: Colors.red),
       home: MyPage(),
@@ -23,6 +26,17 @@ class MyPage extends StatelessWidget {
         title: Text('Snack Bar'),
         centerTitle: true,
       ),
+      body: Center(
+          child: ElevatedButton(
+        child: Text(
+          'Show me',
+          style: TextStyle(color: Colors.white),
+        ),
+        onPressed: () {
+          ScaffoldMessenger.of(context)
+              .showSnackBar(SnackBar(content: Text('Hello')));
+        },
+      )),
     );
   }
 }
