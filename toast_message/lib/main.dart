d0d442a88efbc609a8d6eda12295896d61c13dac
yegri <exsilver305@gmail.com>
2023-05-25 13:40:26 +0900

    toast message 초기 셋팅

---
+++ toast_message/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Toast Message',
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  const MyPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Toast message'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
