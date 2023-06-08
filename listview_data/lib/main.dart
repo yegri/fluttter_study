e545e4a2e1c3bcfcda75ddb7d6b3c6f7eb4269c8
yegri <exsilver305@gmail.com>
2023-06-08 18:20:17 +0900

    초기 셋팅

---
+++ listview_data/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const myApp());
+}
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
