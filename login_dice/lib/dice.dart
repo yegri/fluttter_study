116922acec139773d4229817e2f0709e0009299e
yegri <exsilver305@gmail.com>
2023-06-12 13:46:04 +0900

    Feat : 랜덤 주사위 완료

--- login_dice/lib/dice.dart
+++ login_dice/lib/dice.dart
@@ -1,10 +1,73 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:fluttertoast/fluttertoast.dart';
 
-class Dice extends StatelessWidget {
+class Dice extends StatefulWidget {
   const Dice({super.key});
 
   @override
+  State<Dice> createState() => _DiceState();
+}
+
+class _DiceState extends State<Dice> {
+  int leftDice = 1;
+  int rightDice = 2;
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.redAccent,
+      appBar: AppBar(
+        backgroundColor: Colors.redAccent,
+        title: Text('Dice game'),
+      ),
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Padding(
+            padding: EdgeInsets.all(32.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(child: Image.asset('assets/images/dice$leftDice.png')),
+                SizedBox(
+                  width: 20,
+                ),
+                Expanded(child: Image.asset('assets/images/dice$rightDice.png'))
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 60,
+          ),
+          ButtonTheme(
+            child: ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  leftDice = Random().nextInt(6) + 1;
+                  rightDice = Random().nextInt(6) + 1;
+                });
+                showToast("Left dice: $leftDice, Right dice: $rightDice");
+              },
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.orangeAccent,
+                  fixedSize: const Size(100, 60)),
+              child: Icon(
+                Icons.play_arrow,
+                color: Colors.white,
+                size: 50,
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
   }
+}
+
+void showToast(String message) {
+  Fluttertoast.showToast(
+      msg: message,
+      backgroundColor: Colors.white,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM);
 }
